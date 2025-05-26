bf282118bf43ce6e341de5a4dfadcf1829e0da40
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-05-26 11:42:25 +0200

    add link to img and title on ProjectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,17 +6,25 @@ export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <ScrollReveal>
       <div className="flex flex-col items-center gap-8 md:flex-row md:gap-24">
-        <img
-          src={project.image}
-          alt={project.alt}
-          className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]"
-        />
+        <a
+          href={project.link}
+          target="blank"
+          className="block w-full md:w-[300px] flex-shrink-0"
+        >
+          <img
+            src={project.image}
+            alt={project.alt}
+            className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105"
+          />
+        </a>
         <div className="flex flex-col gap-5">
           <div className="flex flex-col gap-3">
-            <div className="text-xl font-semibold">{project.title}</div>
+            <a href={project.link} target="blank">
+              <div className="text-xl font-semibold">{project.title}</div>
+            </a>
             <div className="flex flex-row gap-2">
               <p>Go :</p>
-              <a href={project.link} target="blank">
+              <a href={project.link} target="blank" className="w-fit">
                 <BsLink className="text-2xl text-white transition-all duration-300 hover:scale-125 hover:text-indigo-500" />
               </a>
             </div>
