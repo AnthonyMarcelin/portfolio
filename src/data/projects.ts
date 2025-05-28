34a724d198c5d9c04e8086e66c0c36d5e8f82221
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-05-28 11:20:55 +0200

    add projects in db

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ const projects = [
     title: "Mon portfolio",
     link: "https://github.com/AnthonyMarcelin/portfolio",
     description: "Création de mon portfolio",
-    techno: ["React", "Tailwind"],
+    techno: ["React", "Tailwind", "Typescript"],
   },
   {
     image: "/images/API_spirit.webp",
@@ -24,6 +24,31 @@ const projects = [
     description:
       "Projet en HTML / CSS effectué en formation en responsive / mobile first",
     techno: ["HTML", "CSS"],
+  },
+  {
+    image: "/images/ocoffee.webp",
+    alt: "photo du projet",
+    title: "O'Coffee",
+    link: "https://github.com/AnthonyMarcelin/Ocoffee",
+    description: "Projet d'une boutique de café en EJS effectué en formation",
+    techno: ["EJS"],
+  },
+  {
+    image: "/images/oquiz.webp",
+    alt: "photo du projet",
+    title: "O'Quiz",
+    link: "https://github.com/AnthonyMarcelin/Oquiz",
+    description: "Projet d'une plateforme de quiz en EJS effectué en formation",
+    techno: ["EJS", "PostgreSQL"],
+  },
+  {
+    image: "/images/Oresto.webp",
+    alt: "photo du projet",
+    title: "O'Resto",
+    link: "https://github.com/AnthonyMarcelin/Oresto-React-Native",
+    description:
+      "Projet d'une plateforme de restaurants en React-native effectué en formation",
+    techno: ["React-native", "Typescript"],
   },
 ];
 
