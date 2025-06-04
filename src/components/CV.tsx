c218eb05e5f9b137d69c807bf7ed999b045676fd
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-06-04 11:58:20 +0200

    CV modified

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -6,7 +6,7 @@ const CV = () => {
       <div className="flex min-h-[40vh] w-full flex-col items-center justify-center gap-8">
         <h1 className="text-4xl font-light text-white md:text-6xl">Mon CV</h1>
         <a
-          href="/src/data/CV Anthony Marcelin.pdf"
+          href="/src/data/CV-anthony-marcelin.pdf"
           download
           className="rounded-full bg-gradient-to-r from-purple-500 to-indigo-500 px-8 py-3 text-white transition-all duration-300 hover:scale-110"
         >
